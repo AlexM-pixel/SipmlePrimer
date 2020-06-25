1650c20172bcc6158457b0a21936ab2d1f01e2f5
Alex_Manego <aleksandrmanego@gmail.com>
2020-06-25 18:31:52 +0300

    ok, this really final retrofit

--- app/src/main/java/com/example/mysympleapplication/hw8/Main8Activity.java
+++ app/src/main/java/com/example/mysympleapplication/hw8/Main8Activity.java
@@ -19,6 +19,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -28,9 +29,13 @@ import android.widget.Toast;
 import com.example.mysympleapplication.R;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -43,7 +48,6 @@ public class Main8Activity extends AppCompatActivity {
     SampleWeather spain;
     SampleWeather minsk;
     LocationManager locationManager;
-    LocationListener locationListener;
     Location myLocation;
     WeatherService weatherService;
     WeatherSpainReceiver weatherSpainReceiver;
@@ -53,13 +57,14 @@ public class Main8Activity extends AppCompatActivity {
     public static final String LOCATION_LAT = "lat_location";
     public static final String LOCATION_LON = "lon_location";
     boolean minskGet;
+    boolean getGeocoder;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         weatherService = MyRetrofit.getInstance().create(WeatherService.class);
         textView_Minsk = findViewById(R.id.textView_Minsk);
         textView_Ispan = findViewById(R.id.textView_Ispan);
@@ -76,7 +81,10 @@ public class Main8Activity extends AppCompatActivity {
                     return;
                 }
                 myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                Toast.makeText(Main8Activity.this, "onClick", Toast.LENGTH_SHORT).show();
                 if (myLocation != null) {
+                    textView_Minsk.setText("ждем...");
+                    Toast.makeText(Main8Activity.this, "myLocation != null", Toast.LENGTH_SHORT).show();
                     final String lat = String.valueOf(myLocation.getLatitude());
                     final String lon = String.valueOf(myLocation.getLongitude());
                     getWeather(lat, lon);
@@ -88,12 +96,17 @@ public class Main8Activity extends AppCompatActivity {
                             try {
                                 Log.d("!!!", "geocoder_RUN");
                                 addressList = geocoder.getFromLocation(Double.parseDouble(lat), Double.parseDouble(lon), 1);
-                                Toast.makeText(Main8Activity.this, addressList.get(0).getCountryName() + " ," + addressList.get(0).getLocality() + " , " + addressList.get(0).getAdminArea(), Toast.LENGTH_SHORT).show();
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         }
                     });
+                    service.shutdown();
+                    try {
+                        getGeocoder=  service.awaitTermination(2,TimeUnit.SECONDS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     Intent intentSpain = new Intent(Main8Activity.this, GeoService.class);
                     intentSpain.putExtra(LOCATION_LAT, "41.23");
                     intentSpain.putExtra(LOCATION_LON, "2.10");
@@ -103,43 +116,46 @@ public class Main8Activity extends AppCompatActivity {
                 //  String lon = "27.525773";
             }
         });
-
+        if (ActivityCompat.checkSelfPermission(Main8Activity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(Main8Activity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 0, 10, locationListener);
     }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            myLocation = location;
+            Log.d("!!!", "onLocationChanged");
+            Toast.makeText(Main8Activity.this, "onLocationChanged", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     protected void onStart() {
         super.onStart();
         weatherSpainReceiver = new WeatherSpainReceiver();
         registerReceiver(weatherSpainReceiver, new IntentFilter(GeoService.SEND_RESPONSE_BODY));
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                myLocation = location;
-                Log.d("!!!", "onLocationChanged");
-                Toast.makeText(Main8Activity.this, "onLocationChanged", Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-                if (ActivityCompat.checkSelfPermission(Main8Activity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(Main8Activity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                myLocation = locationManager.getLastKnownLocation(provider);
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
     }
+
 
     @Override
     protected void onStop() {
@@ -218,18 +234,24 @@ public class Main8Activity extends AppCompatActivity {
 
     @SuppressLint({"DefaultLocale", "SetTextI18n"})
     private void comparisonTemp() {
-        if (minskGet) {
-            textView_Minsk.setText("в " + addressList.get(0).getCountryName() + " " + addressList.get(0).getAdminArea() + " " + minsk.fact.temp + " градусов");
-            textView_Ispan.setText(String.format("а в Испании сейчас: %d градусов", spain.fact.temp));
-            if (spain.fact.temp < 32 && minsk.fact.temp < 20) {
-                buttonSv.setVisibility(View.VISIBLE);
-                buttonSv.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startingSvalivator();
-                    }
-                });
+       do {
+           Log.d("!!!", "do " + " , " + getGeocoder);
+           if (minskGet && addressList != null) {
+               Toast.makeText(this, "if " + getGeocoder + "  , " + minskGet, Toast.LENGTH_SHORT).show();
+               // textView_Minsk.setText("в Минске сейчас: " + minsk.fact.temp + " градусов");
+                textView_Minsk.setText("в " + addressList.get(0).getCountryName() + " " + addressList.get(0).getAdminArea() + " " + minsk.fact.temp + " градусов");
+                textView_Ispan.setText("а в Испании сейчас: " + spain.fact.temp + " градусов");
+                Log.d("!!!", "minskGet && addressList != null" + " , " + getGeocoder);
+                if (spain.fact.temp < 32 && minsk.fact.temp < 20) {
+                    buttonSv.setVisibility(View.VISIBLE);
+                    buttonSv.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startingSvalivator();
+                        }
+                    });
+                }
             }
-        }
+        } while (!getGeocoder);
     }
 }
