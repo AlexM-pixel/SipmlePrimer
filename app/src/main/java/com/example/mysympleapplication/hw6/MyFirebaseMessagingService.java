f2845a694c1a280b435d9bd22f8b7354298fa081
Alex_Manego <aleksandrmanego@gmail.com>
2020-10-27 21:53:10 +0300

    update gradle

--- app/src/main/java/com/example/mysympleapplication/hw6/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/mysympleapplication/hw6/MyFirebaseMessagingService.java
@@ -1,23 +1,9 @@
 package com.example.mysympleapplication.hw6;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
-import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
-import com.example.mysympleapplication.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -37,7 +23,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "NEW_TOKEN: " + token);      // отсюда токен брал для отправки именно моему телефону
     }
 
-    private  void setIntentSendMessage(String notification) {
+    private void setIntentSendMessage(String notification) {
         Intent intent = new Intent(this, ChatActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.putExtra(SET_NOTIFICATION, notification);
